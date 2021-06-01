335ad2692e78bdac0005174174de8359cf1f69ab
nikita1803 <rainikita595@gmail.com>
2021-06-01 14:32:35 +0530

    Added: Append the elements of the linked list

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -35,4 +35,20 @@ public class MyLinkedListTest
 						 myLinkedList.tail.equals(myFirstNode);
 		Assert.assertTrue(result);
 	}
+	@Test
+	public void given3NumberWhenAddedToLinkedListShouldBeAddedToLast()
+	{
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.append(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(myFirstNode) &&
+						 myLinkedList.head.getNext().equals(mySecondNode) &&
+						 myLinkedList.tail.equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
 }
