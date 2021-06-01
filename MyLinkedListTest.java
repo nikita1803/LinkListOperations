847db000f30036d3800c9089d5bc436b4c9d756e
nikita1803 <rainikita595@gmail.com>
2021-06-01 14:06:20 +0530

    Added: Simply created a linked list of 3 numbers

--- MyLinkedListTest.java
+++ MyLinkedListTest.java
@@ -16,6 +16,23 @@ public class MyLinkedListTest
 		myLinkedList.add(mySecondNode);
 		myLinkedList.add(myThirdNode);
 		myLinkedList.printMyNodes();
+		/*boolean result = myLinkedList.head.equals(myThirdNode) &&
+						 myLinkedList.head.getNext().equals(mySecondNode) &&
+						 myLinkedList.tail.equals(myFirstNode);
+		Assert.assertTrue(result);*/
+	}
+	
+	@Test
+	public void given3NumberWhenAddedToLinkedListShouldBeAddedToTop()
+	{
+		MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+		MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+		MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.add(mySecondNode);
+		myLinkedList.add(myThirdNode);
+		myLinkedList.printMyNodes();
 		boolean result = myLinkedList.head.equals(myThirdNode) &&
 						 myLinkedList.head.getNext().equals(mySecondNode) &&
 						 myLinkedList.tail.equals(myFirstNode);
